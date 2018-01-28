482f0d7b35f1dea857339883f2d037672dcf89f7
Roberto Gómez <rob.gomez.mu@gmail.com>
2018-01-28 02:08:44 +0100

    Finish curry example

--- Curry-Swift.playground/Contents.swift
+++ Curry-Swift.playground/Contents.swift
@@ -5,20 +5,41 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+enum Genders: String {
+    case rock = "Rock"
+    case pop = "Pop"
+}
+
+enum Singers: String {
+    case elvis_presley = "197443"
+    case bruce_springsteen = "178834"
+    case bob_dylan = "462006"
+    case frank_sinatra = "171366"
+    case beyonce = "1419227"
+    case freddie_mercury = "3915743"
+    case michael_jackson = "32940"
+}
+
 struct Artist {
     let id: Int
     let name: String
     let gender: String
     let albums: [Album]?
     
-    static func create(id: Int) -> (String) -> (String) -> ([Album]?) -> Artist {
+    static func create(gender: String) -> (String) -> (Int) -> ([Album]?) -> Artist {
         return { name in
-            return { gender in
+            return { id in
                 return { album in
                     Artist(id: id, name: name, gender: gender, albums: album)
                 }
             }
         }
+    }
+    
+    func printArtist() {
+        print("Name:\(name), Gender:\(gender)")
+        print("Albums:")
+        albums?.forEach{ $0.printAlbum() }
     }
 }
 
@@ -29,10 +50,10 @@ struct Album {
     let gender: String
     let image: String
     
-    static func create(id: Int) -> (String) -> (String) -> (String) -> (String) -> Album {
-        return { name in
-            return { copyright in
-                return { gender in
+    static func create(gender: String) -> (Int) -> (String) -> (String) -> (String) -> Album {
+        return { id in
+            return { name in
+                return { copyright in
                     return { image in
                         Album(id: id, name: name, copyright: copyright, gender: gender, image: image)
                     }
@@ -40,8 +61,16 @@ struct Album {
             }
         }
     }
+    
+    func printAlbum() {
+        print("   \(name), \(gender), \(copyright)")
+    }
 }
 
+let createRockArtist = Artist.create(gender: Genders.rock.rawValue)
+let createPopArtist = Artist.create(gender: Genders.pop.rawValue)
+let createRockAlbum = Album.create(gender: Genders.rock.rawValue)
+let createPopAlbum = Album.create(gender: Genders.pop.rawValue)
 
 func fetchBestAlbumsOf(artist: String, completion: @escaping (_ artist: Artist) -> Void) {
    
@@ -70,22 +99,8 @@ func fetchBestAlbumsOf(artist: String, completion: @escaping (_ artist: Artist) 
                             return
                     }
                     
-                    let artist = Artist.create(id: id)(name)(gender)
-                    
-                    let albumsArray = results.filter { $0["collectionType"] as? String == "Album" }
-                    
-                    let albums: [Album] = albumsArray.flatMap {
-                        
-                        guard let id = $0["collectionId"] as? Int,
-                              let name = $0["collectionName"] as? String,
-                              let copyright = $0["copyright"] as? String,
-                              let gender = $0["primaryGenreName"] as? String,
-                              let image = $0["artworkUrl100"] as? String else {
-                                return nil
-                        }
-                        
-                        return Album.create(id: id)(name)(copyright)(gender)(image)
-                    }
+                    let artist = gender == Genders.rock.rawValue ? createRockArtist(name)(id) : createPopArtist(name)(id)
+                    let albums = getAlbums(albumsArray: results.filter{ $0["collectionType"] as? String == "Album" } as [[String : AnyObject]])
                     
                     completion(artist(albums))
                 }
@@ -97,8 +112,25 @@ func fetchBestAlbumsOf(artist: String, completion: @escaping (_ artist: Artist) 
     }
 }
 
-fetchBestAlbumsOf(artist: "178834") { print($0) }
+func getAlbums(albumsArray: [[String: AnyObject]]) -> [Album]? {
+    return albumsArray.flatMap {
+        
+        guard let id = $0["collectionId"] as? Int,
+            let name = $0["collectionName"] as? String,
+            let copyright = $0["copyright"] as? String,
+            let gender = $0["primaryGenreName"] as? String,
+            let image = $0["artworkUrl100"] as? String else {
+                return nil
+        }
+        
+        return gender == Genders.rock.rawValue ? createRockAlbum(id)(name)(copyright)(image) : createPopAlbum(id)(name)(copyright)(image)
+    }
+}
 
-fetchBestAlbumsOf(artist: "462006") { print($0) }
-
-
+fetchBestAlbumsOf(artist: Singers.bruce_springsteen.rawValue) { $0.printArtist() }
+fetchBestAlbumsOf(artist: Singers.bob_dylan.rawValue) { $0.printArtist() }
+fetchBestAlbumsOf(artist: Singers.beyonce.rawValue) { $0.printArtist() }
+fetchBestAlbumsOf(artist: Singers.elvis_presley.rawValue) { $0.printArtist() }
+fetchBestAlbumsOf(artist: Singers.frank_sinatra.rawValue) { $0.printArtist() }
+fetchBestAlbumsOf(artist: Singers.freddie_mercury.rawValue) { $0.printArtist() }
+fetchBestAlbumsOf(artist: Singers.michael_jackson.rawValue) { $0.printArtist() }
